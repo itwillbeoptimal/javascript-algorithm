57dc373974109aeaa58feb8a72696b9ad2340aec
지훈 <153643266+itwillbeoptimal@users.noreply.github.com>
2026-03-23 08:51:58 +0900

    [Gold III] Title: 하늘에서 별똥별이 빗발친다, Time: 184 ms, Memory: 12388 KB -BaekjoonHub

---
+++ 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").trim().split("\n");
+
+const [, , l, k] = input[0].split(" ").map(Number);
+const stars = input.slice(1).map(s => s.split(" ").map(Number));
+
+let answer = Infinity;
+
+stars.forEach(([x]) => {
+  stars.forEach(([, y]) => {
+    let covered = 0;
+    stars.forEach(([sx, sy]) => {
+      if (x <= sx && sx <= x + l && y <= sy && sy <= y + l) {
+        covered += 1;
+      }
+    });
+    answer = Math.min(answer, k - covered);
+  });
+});
+
+console.log(answer);
