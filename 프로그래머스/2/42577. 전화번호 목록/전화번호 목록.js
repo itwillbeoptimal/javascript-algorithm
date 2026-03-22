e175a6d8646d47e94b0fa7411f5039b3352b5a81
지훈 <153643266+itwillbeoptimal@users.noreply.github.com>
2026-03-22 19:59:09 +0900

    [level 2] Title: 전화번호 목록, Time: 131.11 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,7 @@
+function solution(phoneBook) {
+  phoneBook.sort();
+  for (let i = 0; i < phoneBook.length - 1; i++) {
+    if (phoneBook[i + 1].startsWith(phoneBook[i])) return false;
+  }
+  return true;
+}
